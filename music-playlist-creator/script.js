62dc791303748f7d706df846978f10ab72e79107
Javier Carrillo Zamora <javiercz@meta.com>
2025-06-05 09:56:52 -0700

    Style cards and modal

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,22 @@
+const modal = document.getElementById('modal');
+const content = document.getElementById('modal-cont')
 
+function showModal() {
+    modal.classList.add('show-overlay')
+    content.classList.remove('hide-content')
+}
+
+function hideModal() {
+    modal.classList.remove('show-overlay')
+    content.classList.add('hide-content')
+}
+
+let cards = document.getElementsByClassName('card');
+
+for (let item of cards) {
+    console.log(item)
+    item.addEventListener('click', showModal)
+}
+
+modal.addEventListener('click', hideModal)
+document.getElementById('modal-cont').addEventListener('click', function(event) {event.stopPropagation()})
